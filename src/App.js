5712fcf0561fea47ad7698eff9a612a44e01e893
Ameya Mohape <mohapeameya@gmail.com>
2024-03-21 22:27:17 +0530

    Store inputs in state and crop image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import { useEffect, useState } from "react";
+import ReactModal from 'react-modal';
+import CropPhoto from './components/cropphoto/CropPhoto'
 
 import { rashiValues, complexionValues, heightValues, weightValues } from "./data";
 
 export default function App() {
 
-  const [biodata, setBiodata] = useState({});
+  const [biodata, setBiodata] = useState({
+    name: { checked: true, value: '' },
+    dob: { checked: true, value: '' },
+    tob: { checked: true, value: '' },
+    pob: { checked: true, value: '' },
+    rashi: { checked: true, value: 0 },
+    complexion: { checked: true, value: 0 },
+    height: { checked: true, value: 25 },
+    weight: { checked: true, value: 0 },
+    education: { checked: true, value: '' },
+    job: { checked: true, value: '' },
+    religionCaste: { checked: true, value: '' },
+    father: { checked: true, value: '' },
+    fatherJob: { checked: true, value: '' },
+    mother: { checked: true, value: '' },
+    motherJob: { checked: true, value: '' },
+    contact: { checked: true, value: '' },
+    address: { checked: true, value: '' },
+    image: { checked: true, value: '' },
+    filename: { checked: true, value: '' }
+  });
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleToggleModal = () => {
+    setShowModal((prevState) => !prevState);
+  }
+
+  const setImage = (imageObject) => {
+    setBiodata({
+      ...biodata,
+      image: { checked: true, value: imageObject.image },
+      filename: { checked: true, value: imageObject.filename }
+    })
+  }
 
   useEffect(() => {
     console.log('App component loaded');
@@ -30,7 +66,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Full Name</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" placeholder="Enter full name" />
+                  <input className="form-control" placeholder="Enter full name" onChange={e => setBiodata({ ...biodata, name: { checked: biodata.name.checked, value: e.target.value } })} />
                 </div>
               </div>
             </div>
@@ -48,7 +84,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Date of Birth</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" aria-label="Date" type="date" />
+                  <input className="form-control" aria-label="Date" type="date" onChange={e => setBiodata({ ...biodata, dob: { checked: biodata.dob.checked, value: e.target.value } })} />
                 </div>
               </div>
             </div>
@@ -66,7 +102,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Time of Birth</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" aria-label="Time" type="time" />
+                  <input className="form-control" aria-label="Time" type="time" onChange={e => setBiodata({ ...biodata, tob: { checked: biodata.tob.checked, value: e.target.value } })} />
                 </div>
               </div>
             </div>
@@ -78,13 +114,13 @@ export default function App() {
             <div className="col-lg-8">
               <div className="row align-items-center p-1">
                 <div className="col-auto form-switch">
-                  <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" />
+                  <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" onChange={e => setBiodata({ ...biodata, pob: { checked: e.target.value, value: biodata.pob.value } })} />
                 </div>
                 <div className="col text-start">
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Place of Birth</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" placeholder="Enter place of birth" />
+                  <input className="form-control" placeholder="Enter place of birth" onChange={e => setBiodata({ ...biodata, pob: { checked: biodata.pob.checked, value: e.target.value } })} />
                 </div>
               </div>
             </div>
@@ -102,7 +138,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Rashi</label>
                 </div>
                 <div className="col text-start">
-                  <select name="rashi" id="rashi" className="form-select">
+                  <select name="rashi" id="rashi" className="form-select" onChange={e => setBiodata({ ...biodata, rashi: Number(e.target.value) })}>
                     {
                       rashiValues.map((item, index) => <option key={item} value={index}>{item}</option>)
                     }
@@ -124,7 +160,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Complexion</label>
                 </div>
                 <div className="col text-start">
-                  <select name="rashi" id="complexion" className="form-select">
+                  <select name="rashi" id="complexion" className="form-select" onChange={e => setBiodata({ ...biodata, complexion: Number(e.target.value) })}>
                     {
                       complexionValues.map((item, index) => <option key={item} value={index}>{item}</option>)
                     }
@@ -146,7 +182,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Height</label>
                 </div>
                 <div className="col text-start">
-                  <select name="rashi" id="height" className="form-select">
+                  <select name="rashi" id="height" className="form-select" onChange={e => setBiodata({ ...biodata, height: Number(e.target.value) })}>
                     {
                       heightValues.map((item, index) => <option key={item} value={index}>{item}</option>)
                     }
@@ -168,7 +204,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Weight</label>
                 </div>
                 <div className="col text-start">
-                  <select name="rashi" id="weight" className="form-select">
+                  <select name="rashi" id="weight" className="form-select" onChange={e => setBiodata({ ...biodata, weight: Number(e.target.value) })}>
                     {
                       weightValues.map((item, index) => <option key={item} value={index}>{item}</option>)
                     }
@@ -190,7 +226,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Education</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" placeholder="Enter highest education" />
+                  <input className="form-control" placeholder="Enter highest education" onChange={e => setBiodata({ ...biodata, education: e.target.value })} />
                 </div>
               </div>
             </div>
@@ -208,7 +244,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Job/Occupation</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" placeholder="Enter job details" />
+                  <input className="form-control" placeholder="Enter job details" onChange={e => setBiodata({ ...biodata, job: e.target.value })} />
                 </div>
               </div>
             </div>
@@ -226,7 +262,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Religion/Caste</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" placeholder="Enter religion and caste" />
+                  <input className="form-control" placeholder="Enter religion and caste" onChange={e => setBiodata({ ...biodata, religionCaste: e.target.value })} />
                 </div>
               </div>
             </div>
@@ -250,7 +286,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Father's Name</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" placeholder="Enter father's full name" />
+                  <input className="form-control" placeholder="Enter father's full name" onChange={e => setBiodata({ ...biodata, father: e.target.value })} />
                 </div>
               </div>
             </div>
@@ -268,7 +304,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Father's Occupation</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" placeholder="Enter father's occupation" />
+                  <input className="form-control" placeholder="Enter father's occupation" onChange={e => setBiodata({ ...biodata, fatherJob: e.target.value })} />
                 </div>
               </div>
             </div>
@@ -286,7 +322,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Mother's Name</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" placeholder="Enter mother's full name" />
+                  <input className="form-control" placeholder="Enter mother's full name" onChange={e => setBiodata({ ...biodata, mother: e.target.value })} />
                 </div>
               </div>
             </div>
@@ -304,7 +340,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Mother's Occupation</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" placeholder="Enter mother's occupation" />
+                  <input className="form-control" placeholder="Enter mother's occupation" onChange={e => setBiodata({ ...biodata, motherJob: e.target.value })} />
                 </div>
               </div>
             </div>
@@ -328,7 +364,7 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Contact</label>
                 </div>
                 <div className="col text-start">
-                  <input className="form-control" placeholder="Enter contact number" />
+                  <input className="form-control" placeholder="Enter contact number" onChange={e => setBiodata({ ...biodata, contact: e.target.value })} />
                 </div>
               </div>
             </div>
@@ -346,13 +382,52 @@ export default function App() {
                   <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Address</label>
                 </div>
                 <div className="col text-start">
-                  <textarea className="form-control" rows="3" placeholder="Enter current address" />
+                  <textarea className="form-control" rows="3" placeholder="Enter current address" onChange={e => setBiodata({ ...biodata, address: e.target.value })} />
                 </div>
               </div>
             </div>
             <div className="col"></div>
           </div>
 
+        </div>
+      </section>
+
+      <ReactModal isOpen={showModal} ariaHideApp={false}>
+        <CropPhoto closeModal={handleToggleModal} handleCroppedImage={setImage} />
+      </ReactModal>
+
+      <section className="fluid-container text-center">
+        <div className="container shadow p-3 mb-3 bg-white rounded">
+          {/* <h3>Add Photo</h3> */}
+          <div className="row">
+            <div className="col"></div>
+            <div className="col-lg-8">
+              <div className="row align-items-center p-1">
+                <div className="col">
+                  <button className="btn btn-secondary" onClick={handleToggleModal}>
+                    {biodata.image.value ? (<span>Replace Photo</span>) : (<span>Add Photo</span>)}
+                  </button>
+                </div>
+                <div className="col">
+                  <button className="btn btn-primary" onClick={() => { }}>Preview</button>
+                </div>
+              </div>
+            </div>
+            <div className="col"></div>
+          </div>
+          <div className="row">
+            <div className="col"></div>
+            <div className="col-lg-8">
+              <div className="row align-items-center p-1">
+                <div className="col">
+                  {biodata.filename.value && (<div><span>{biodata.filename.value}</span></div>)}
+                </div>
+                <div className="col">
+                </div>
+              </div>
+            </div>
+            <div className="col"></div>
+          </div>
         </div>
       </section>
     </>
---
+++ src/components/cropphoto/CropPhoto.js
@@ -0,0 +1,279 @@
+import React, { useState, useRef } from 'react'
+
+import ReactCrop, {
+  centerCrop,
+  makeAspectCrop,
+  convertToPixelCrop,
+} from 'react-image-crop'
+import { canvasPreview } from './canvasPreview'
+import { useDebounceEffect } from './useDebounceEffect'
+import imageCompression from 'browser-image-compression';
+
+import 'react-image-crop/dist/ReactCrop.css'
+
+// This is to demonstate how to make and center a % aspect crop
+// which is a bit trickier so we use some helper functions.
+function centerAspectCrop(mediaWidth, mediaHeight, aspect) {
+  return centerCrop(
+    makeAspectCrop(
+      {
+        unit: '%',
+        width: 90,
+      },
+      aspect,
+      mediaWidth,
+      mediaHeight,
+    ),
+    mediaWidth,
+    mediaHeight,
+  )
+}
+
+export default function CropPhoto({ closeModal = () => { }, handleCroppedImage = () => { } }) {
+  const [imgSrc, setImgSrc] = useState('')
+  const previewCanvasRef = useRef(null)
+  const imgRef = useRef(null)
+  const hiddenAnchorRef = useRef(null)
+  const blobUrlRef = useRef('')
+  const [crop, setCrop] = useState()
+  const [completedCrop, setCompletedCrop] = useState()
+  const [scale, setScale] = useState(1)
+  const [rotate, setRotate] = useState(0)
+  const [aspect, setAspect] = useState(8 / 10)
+  const [filename, setFileName] = useState('');
+
+  async function onSelectFile(event) {
+    if (event.target.files && event.target.files.length > 0) {
+      setCrop(undefined) // Makes crop preview update between images.
+      const imageFile = event.target.files[0];
+      const options = {
+        maxSizeMB: 0.256,
+        maxWidthOrHeight: 1920
+      }
+      try {
+        const compressedFile = await imageCompression(imageFile, options);
+        const reader = new FileReader()
+        reader.addEventListener('load', () =>
+          setImgSrc(reader.result?.toString() || ''),
+        )
+        reader.readAsDataURL(compressedFile)
+        setFileName(event.target.files[0]?.name);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  function onImageLoad(e) {
+    if (aspect) {
+      const { width, height } = e.currentTarget
+      setCrop(centerAspectCrop(width, height, aspect))
+    }
+  }
+
+  async function onDownloadCropClick() {
+    const image = imgRef.current
+    const previewCanvas = previewCanvasRef.current
+    if (!image || !previewCanvas || !completedCrop) {
+      throw new Error('Crop canvas does not exist')
+    }
+
+    // This will size relative to the uploaded image
+    // size. If you want to size according to what they
+    // are looking at on screen, remove scaleX + scaleY
+    const scaleX = image.naturalWidth / image.width
+    const scaleY = image.naturalHeight / image.height
+
+    const offscreen = new OffscreenCanvas(
+      completedCrop.width * scaleX,
+      completedCrop.height * scaleY,
+    )
+    const ctx = offscreen.getContext('2d')
+    if (!ctx) {
+      throw new Error('No 2d context')
+    }
+
+    ctx.drawImage(
+      previewCanvas,
+      0,
+      0,
+      previewCanvas.width,
+      previewCanvas.height,
+      0,
+      0,
+      offscreen.width,
+      offscreen.height,
+    )
+    // You might want { type: "image/jpeg", quality: <0 to 1> } to
+    // reduce image size
+    const blob = await offscreen.convertToBlob({
+      type: 'image/png',
+    })
+
+    if (blobUrlRef.current) {
+      URL.revokeObjectURL(blobUrlRef.current)
+    }
+    blobUrlRef.current = URL.createObjectURL(blob)
+
+    // dispatch(edit({ type: 'IMAGE', value: blobUrlRef.current }));
+    // dispatch(edit({ type: 'FILENAME', value: filename }));
+    const imageObject = {
+      filename: filename,
+      image: blobUrlRef.current
+    }
+    handleCroppedImage(imageObject);
+    closeModal();
+  }
+
+  useDebounceEffect(
+    async () => {
+      if (
+        completedCrop?.width &&
+        completedCrop?.height &&
+        imgRef.current &&
+        previewCanvasRef.current
+      ) {
+        // We use canvasPreview as it's much faster than imgPreview.
+        canvasPreview(
+          imgRef.current,
+          previewCanvasRef.current,
+          completedCrop,
+          scale,
+          rotate,
+        )
+      }
+    },
+    100,
+    [completedCrop, scale, rotate],
+  )
+
+  function handleToggleAspectClick() {
+    if (aspect) {
+      setAspect(undefined)
+    } else {
+      setAspect(16 / 9)
+
+      if (imgRef.current) {
+        const { width, height } = imgRef.current
+        const newCrop = centerAspectCrop(width, height, 16 / 9)
+        setCrop(newCrop)
+        // Updates the preview
+        setCompletedCrop(convertToPixelCrop(newCrop, width, height))
+      }
+    }
+  }
+
+  return (
+    <div className='fluid-container text-center'>
+
+      {!imgSrc && (
+        <>
+          <div className='row'>
+            <div className='col'>
+              <input className='form-control' type="file" accept="image/*" onChange={onSelectFile} />
+            </div>
+            <div className='col'>
+              <button className="btn btn-secondary" onClick={closeModal}>Cancel</button>
+            </div>
+          </div>
+        </>
+      )}
+
+      {/* <div>
+          <label htmlFor="scale-input">Scale: </label>
+          <input
+            id="scale-input"
+            type="number"
+            step="0.1"
+            value={scale}
+            disabled={!imgSrc}
+            onChange={(e) => setScale(Number(e.target.value))}
+          />
+        </div> */}
+      {/* <div>
+          <label htmlFor="rotate-input">Rotate: </label>
+          <input
+            id="rotate-input"
+            type="number"
+            value={rotate}
+            disabled={!imgSrc}
+            onChange={(e) =>
+              setRotate(Math.min(180, Math.max(-180, Number(e.target.value))))
+            }
+          />
+        </div> */}
+      {/* <div>
+          <button onClick={handleToggleAspectClick}>
+            Toggle aspect {aspect ? 'off' : 'on'}
+          </button>
+        </div> */}
+      <div className='row'>
+        <div className='col-md p-3'>
+          {!!imgSrc && (
+            <>
+              <div className='col'>Profile photo</div>
+              <div className='col'>
+                <ReactCrop
+                  crop={crop}
+                  onChange={(_, percentCrop) => setCrop(percentCrop)}
+                  onComplete={(c) => setCompletedCrop(c)}
+                  aspect={aspect}
+                  // minWidth={400}
+                  minHeight={100}
+                // circularCrop
+                >
+                  <img
+                    ref={imgRef}
+                    alt="Crop me"
+                    src={imgSrc}
+                    style={{ transform: `scale(${scale}) rotate(${rotate}deg)` }}
+                    onLoad={onImageLoad}
+                    width="300"
+                  />
+                </ReactCrop>
+              </div>
+            </>
+          )}
+        </div>
+        <div className='col-md p-3'>
+          {!!completedCrop && (
+            <>
+              <div className='col'>Preview photo</div>
+              <div className='col'>
+                <canvas
+                  ref={previewCanvasRef}
+                  style={{
+                    border: '1px solid black',
+                    objectFit: 'contain',
+                    width: 300,
+                    // width: completedCrop.width,
+                    // height: completedCrop.height,
+                  }}
+                />
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+      {!!completedCrop && (
+        <div className='row'>
+          <div>
+            <button className="btn btn-primary" onClick={onDownloadCropClick}>Done</button>
+            <a
+              href="#hidden"
+              ref={hiddenAnchorRef}
+              download
+              style={{
+                position: 'absolute',
+                top: '-200vh',
+                visibility: 'hidden',
+              }}
+            >
+              Hidden download
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
