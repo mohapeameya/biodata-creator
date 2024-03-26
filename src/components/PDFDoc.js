247d07fcb38d372f8dc8b52ef42b002607970f97
Ameya Mohape <mohapeameya@gmail.com>
2024-03-26 23:52:48 +0530

    Show standard date and time formats in pdf

--- src/components/PDFDoc.js
+++ src/components/PDFDoc.js
@@ -17,6 +17,32 @@ export default function PDFDoc({ name, dob, tob, pob, rashi, complexion,
     console.log('Doc component loaded')
   }, [])
 
+  const convertTo12HourFormat = (time24) => {
+    // Parse hours and minutes from the 24-hour time string
+    const [hours, minutes] = time24.split(':').map(Number);
+
+    // Determine AM or PM
+    const period = hours >= 12 ? 'PM' : 'AM';
+
+    // Convert hours to 12-hour format
+    const hours12 = (hours % 12) || 12;
+
+    // Format the time in 12-hour format with AM/PM
+    const time12 = `${hours12}:${minutes < 10 ? '0' : ''}${minutes} ${period}`;
+
+    return time12;
+  }
+
+  const convertDateFormat = (dateString) => {
+    // Split the date string into year, month, and day
+    const [year, month, day] = dateString.split('-');
+
+    // Rearrange the components in the desired format
+    const formattedDate = `${day}/${month}/${year}`;
+
+    return formattedDate;
+  }
+
   return (
     <Document>
       <Page size="A4" style={styles.page}>
@@ -36,12 +62,12 @@ export default function PDFDoc({ name, dob, tob, pob, rashi, complexion,
                 {dob.checked &&
                   <View style={{ flexDirection: 'row' }}>
                     <Text style={personalDetailsFlex.label}>Date of birth:</Text>
-                    <Text style={personalDetailsFlex.value}>{dob.value}</Text>
+                    <Text style={personalDetailsFlex.value}>{convertDateFormat(dob.value)}</Text>
                   </View>}
                 {tob.checked &&
                   <View style={{ flexDirection: 'row' }}>
                     <Text style={personalDetailsFlex.label}>Time of birth:</Text>
-                    <Text style={personalDetailsFlex.value}>{tob.value}</Text>
+                    <Text style={personalDetailsFlex.value}>{convertTo12HourFormat(tob.value)}</Text>
                   </View>}
 
                 {pob.checked &&
